a322631bf314881d92a050f61cd30d5a043b2738
AcJoell <joelac04@hotmail.com>
2021-05-20 14:33:06 -0500

    incorporando estilos

--- code/platzi-badges/src/components/Badge.js
+++ code/platzi-badges/src/components/Badge.js
@@ -1,31 +1,36 @@
 import React from "react";
-import confLogo from "../images/badge-header.svg"
+import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
 
 // Los componentes son clases
 class Badge extends React.Component {
   // render define el resultado que vamos a ver en pantalla
   render() {
     return (
-        <div>
-            <div>
-                <img src={confLogo} alt="Logo de la conferencia"></img> 
-            </div>
-            
-            <div>
-            <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img> 
-                <h1>Joel <br/> Acosta</h1>
-            </div>
-
-            <div>
-                <p>Front-end Developer</p>
-                <p>@AcJoell</p>
-            </div>
-
-            <div>
-                #platziConf
-            </div>
+      <div className="badge">
+        <div className="badge_header">
+          <img src={confLogo} alt="Logo de la conferencia"></img>
         </div>
-    )
+
+        <div className="badge_section-name">
+          <img
+            className="badge_avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          ></img>
+          <h1>
+            Joel <br /> Acosta
+          </h1>
+        </div>
+
+        <div className="badge_section-info">
+          <h4>Front-end Developer</h4>
+          <div>@AcJoell</div>
+        </div>
+
+        <div className="badge_footer">#platziConf</div>
+      </div>
+    );
   }
 }
 
--- code/platzi-badges/src/index.js
+++ code/platzi-badges/src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// npm install bootstap para instalar bootstrap y utilizarlo
+// a dia de hoy (20-05-2021) el archivo a llamar es bootstrap-grid.css
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import './global.css'
+
 // importamos nuestro componente
 import Badge from "./components/Badge";
 
